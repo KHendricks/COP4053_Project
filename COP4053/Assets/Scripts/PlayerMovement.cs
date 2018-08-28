ec9508711bea1db894efec67701a9844d4d6d6c3
Kyle <khendricks0815@gmail.com>
2018-08-28 02:56:08 -0400

    Added player + movement to LevelOne Scene

---
+++ COP4053/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour 
+{
+    public float speed = 3f;
+
+	// Use this for initialization
+	void Start () 
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        Movement();
+	}
+
+    void Movement()
+    {
+        var x = Input.GetAxis("Horizontal");
+        var z = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(x, 0, z) * speed * Time.deltaTime;
+        gameObject.GetComponent<Rigidbody>().MovePosition(transform.position + movement);
+    }
+}
