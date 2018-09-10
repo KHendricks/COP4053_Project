8b63d454e6f15e300820f7b467c8a6e9ad45c3bd
Faye Strawn <strawnfaye@gmail.com>
2018-09-10 00:54:57 -0400

    Imported sprite sheets and reorganized folders

---
+++ COP4053/Assets/Scripts/Billboard.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+    }
+	
+	// Update is called once per frame
+    void Update () {
+        transform.LookAt(Camera.main.transform.position);
+
+        Quaternion rot = transform.rotation;
+        float offset = rot.eulerAngles.y + 180;
+        rot.eulerAngles = new Vector3(rot.eulerAngles.x, offset, rot.eulerAngles.z);
+        transform.rotation = rot;
+    }
+}
