12b0cb627eb7db67d05ba0e96bd54533bcc381f6
Kyle <khendricks0815@gmail.com>
2018-09-11 16:03:29 -0400

    Added DogFollow script
    This script will allow the object attached to the script to follow the "player" and stop following them at a certain distance and will continue following them once that threshold is passed

---
+++ COP4053/Assets/Scripts/DogFollow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DogFollow : MonoBehaviour 
+{
+    public float distanceFromPlayer;
+
+    public GameObject player;
+    public float speed = 3f; 
+
+	// Update is called once per frame
+	void Update () 
+    {
+        // The step size is equal to speed times frame time.
+        float step = speed * Time.deltaTime;
+
+        float distance = Vector3.Distance(transform.position, player.transform.position);
+
+        // Move our position a step closer to the target, if player is far enough away
+        if (distance > distanceFromPlayer)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, player.transform.position, step);
+        }
+    }
+}
