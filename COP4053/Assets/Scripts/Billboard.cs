06b0b0ad08fcd03ee96808628aa2f688aeb1aaea
Faye Strawn <strawnfaye@gmail.com>
2018-09-14 00:21:21 -0400

    Made lil boi a sprite in the scene, movement scripts in progress! Also added some background environment

--- COP4053/Assets/Scripts/Billboard.cs
+++ COP4053/Assets/Scripts/Billboard.cs
@@ -4,18 +4,27 @@ using UnityEngine;
 
 public class Billboard : MonoBehaviour {
 
+    public Transform MyCameraTransform;
+    private Transform MyTransform;
+    public bool alignNotLook = true;
+
 	// Use this for initialization
 	void Start () {
-
+        MyTransform = this.transform;
+        MyCameraTransform = Camera.main.transform;
     }
 	
 	// Update is called once per frame
     void Update () {
-        transform.LookAt(Camera.main.transform.position);
+        if (alignNotLook)
+            MyTransform.forward = MyCameraTransform.forward;
+        else
+            MyTransform.LookAt(MyCameraTransform, Vector3.up);
+        //transform.LookAt(Camera.main.transform.position);
 
-        Quaternion rot = transform.rotation;
-        float offset = rot.eulerAngles.y + 180;
-        rot.eulerAngles = new Vector3(rot.eulerAngles.x, offset, rot.eulerAngles.z);
-        transform.rotation = rot;
+        //Quaternion rot = transform.rotation;
+        //float offset = rot.eulerAngles.y + 180;
+        //rot.eulerAngles = new Vector3(rot.eulerAngles.x, offset, rot.eulerAngles.z);
+        //transform.rotation = rot;
     }
 }
--- COP4053/Assets/Scripts/PlayerMovement.cs
+++ COP4053/Assets/Scripts/PlayerMovement.cs
@@ -2,15 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerMovement : MonoBehaviour 
 {
     public float speed = 3f;
+    Rigidbody rb;
 
 	// Use this for initialization
 	void Start () 
     {
-		
-	}
+        rb = gameObject.GetComponent<Rigidbody>();
+
+    }
 	
 	// Update is called once per frame
 	void Update () 
@@ -20,10 +23,11 @@ public class PlayerMovement : MonoBehaviour
 
     void Movement()
     {
-        var x = Input.GetAxis("Horizontal");
-        var z = Input.GetAxis("Vertical");
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(x, 0, z) * speed * Time.deltaTime;
-        gameObject.GetComponent<Rigidbody>().MovePosition(transform.position + movement);
+        // Vector3 movement = new Vector3(x, 0, z) * speed * Time.deltaTime;
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement * speed);
     }
 }
