0f74ad6e2bda6b89eb8d9530cb9a3b74a7999fd6
Faye Strawn <strawnfaye@gmail.com>
2018-09-18 17:51:50 -0400

    trying out animation stuff

--- COP4053/Assets/Scripts/PlayerMovement.cs
+++ COP4053/Assets/Scripts/PlayerMovement.cs
@@ -71,6 +71,8 @@ public class PlayerMovement : MonoBehaviour
         forward.Normalize();
         right.Normalize();
 
+        GetAngleIndex(forward);
+
         // Direction we want to move;
         var dir = forward * verticalAxis + right * horizontalAxis;
 
@@ -86,5 +88,31 @@ public class PlayerMovement : MonoBehaviour
             speed *= sprintMultiplier;
         else
             speed /= sprintMultiplier;
+    }
+
+    int GetAngleIndex(Vector3 forward)
+    {
+        var dir = Camera.main.transform.position - forward;
+        var playerAngle = Mathf.Atan2(dir.z, dir.x) * Mathf.Rad2Deg;
+        if (playerAngle < 0.0f)
+            playerAngle += 360;
+        Debug.Log("Angle from the player is: " + playerAngle);
+        if (playerAngle >= 292.5f && playerAngle < 337.5f)
+            return 8;
+        else if (playerAngle >= 22.5f && playerAngle < 67.5f)
+            return 2;
+        else if (playerAngle >= 67.5f && playerAngle < 112.5f)
+            return 3;
+        else if (playerAngle >= 112.5f && playerAngle < 157.5f)
+            return 4;
+        else if (playerAngle >= 157.5f && playerAngle < 202.5f)
+            return 5;
+        else if (playerAngle >= 202.5f && playerAngle < 247.5f)
+            return 6;
+        else if (playerAngle >= 247.5f && playerAngle < 292.5f)
+            return 7;
+        else if (playerAngle >= 337.5f || playerAngle < 22.5f)
+            return 1;
+        else return 0;
     }
 }
