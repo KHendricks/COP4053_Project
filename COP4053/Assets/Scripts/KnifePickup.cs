7346c7f492abf252201f0afba42a50a1a46fb609
Kyle <khendricks0815@gmail.com>
2018-10-02 15:01:19 -0400

    Added image pickup
    Using stock image assets for now, updated selector UI

---
+++ COP4053/Assets/Scripts/KnifePickup.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnifePickup : MonoBehaviour 
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            PlayerPrefs.SetInt("Knife", 1);
+            Destroy(gameObject);
+        }
+    }
+}
--- COP4053/Assets/Scripts/PlayerInventory.cs
+++ COP4053/Assets/Scripts/PlayerInventory.cs
@@ -28,6 +28,78 @@ public class PlayerInventory : MonoBehaviour
         LoadInventory();
 	}
 
+    // Update is called once per frame
+    void Update()
+    {
+        // This function allows for Q + E to rotate the selector
+        ChangeSelection();
+
+        // This function updates the inventory UI if the player picks up a 
+        // corresponding inventory item
+        LoadInventory();
+
+        // This function is to allow the player to display the current
+        // item selected
+        // TODO: This is a WIP
+        DisplayCurrent();
+    }
+
+    void DisplayCurrent()
+    {
+        // This number will hold the index of the inventory
+        // 0: Lasso
+        // 1: Knife
+        // 2: Whip
+        // 3: Boomerang
+        // 4: Slingshot
+        int selectorIndex = PlayerPrefs.GetInt("InventorySlotSelected");
+
+        switch(selectorIndex)
+        {
+            // Lasso
+            case 0:
+                // Need to check if slot is active first (this is to ensure the
+                // the player has the item)
+                if (!inventorySlots[0].activeSelf)
+                {
+                    break;
+                }
+                break;
+
+            // Knife
+            case 1:
+                if (!inventorySlots[1].activeSelf)
+                {
+                    break;
+                }
+                break;
+
+            // Whip
+            case 2:
+                if (!inventorySlots[2].activeSelf)
+                {
+                    break;
+                }
+                break;
+
+            // Boomerang
+            case 3:
+                if (!inventorySlots[3].activeSelf)
+                {
+                    break;
+                }
+                break;
+
+            // Slingshot
+            case 4:
+                if (!inventorySlots[4].activeSelf)
+                {
+                    break;
+                }
+                break;
+        }
+    }
+
     void LoadInventory()
     {
         // Loads the inventory UI based on the playerprefs
@@ -76,12 +148,6 @@ public class PlayerInventory : MonoBehaviour
             inventorySlots[4].SetActive(false);
         }
     }
-
-	// Update is called once per frame
-	void Update () 
-    {
-        ChangeSelection();
-	}
 
     void ChangeSelection()
     {
