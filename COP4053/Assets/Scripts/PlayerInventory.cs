271ad25dbf09c5cafda31fc95d78781cc2b734ab
Kyle <khendricks0815@gmail.com>
2018-10-02 12:46:37 -0400

    Set up inventory UI
    Each slot has a playerpreference to associate with it. Once the player picks up an item, I will update the playerpref to 1 to signal he has the item and adjust that to the inventory slot. Still need to add selecting functionality and pickup functionality to the inventory

---
+++ COP4053/Assets/Scripts/PlayerInventory.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerInventory : MonoBehaviour 
+{
+    public GameObject[] inventorySlots;
+
+	// Use this for initialization
+	void Start ()
+    {
+        // If on first level loads the values for the inventory to empty
+        if (SceneManager.GetActiveScene().name == "Desert_level1")
+        {
+            PlayerPrefs.SetInt("Lasso", 0);
+            PlayerPrefs.SetInt("Knife", 0);
+            PlayerPrefs.SetInt("Whip", 0);
+            PlayerPrefs.SetInt("Boomerang", 0);
+            PlayerPrefs.SetInt("Slingshot", 0);
+        }
+
+        LoadInventory();
+	}
+
+    void LoadInventory()
+    {
+        // Loads the inventory UI based on the playerprefs
+        if (PlayerPrefs.GetInt("Lasso") == 1)
+        {
+            inventorySlots[0].SetActive(true);
+        }
+        else
+        {
+            inventorySlots[0].SetActive(false);
+        }
+
+        if (PlayerPrefs.GetInt("Knife") == 1)
+        {
+            inventorySlots[1].SetActive(true);
+        }
+        else
+        {
+            inventorySlots[1].SetActive(false);
+        }
+
+        if (PlayerPrefs.GetInt("Whip") == 1)
+        {
+            inventorySlots[2].SetActive(true);
+        }
+        else
+        {
+            inventorySlots[2].SetActive(false);
+        }
+
+        if (PlayerPrefs.GetInt("Boomerang") == 1)
+        {
+            inventorySlots[3].SetActive(true);
+        }
+        else
+        {
+            inventorySlots[3].SetActive(false);
+        }
+
+        if (PlayerPrefs.GetInt("Slingshot") == 1)
+        {
+            inventorySlots[4].SetActive(true);
+        }
+        else
+        {
+            inventorySlots[4].SetActive(false);
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
