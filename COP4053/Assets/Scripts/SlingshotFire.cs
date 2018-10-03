2125e4a9480bfa24bd5de2120ae9c03f3ed4109b
Kyle <khendricks0815@gmail.com>
2018-10-03 10:17:25 -0400

    Began work on slingshot weapon
    Its currently set up as a prefab

---
+++ COP4053/Assets/Scripts/SlingshotFire.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlingshotFire : MonoBehaviour 
+{
+    // Have the rock prefab loaded into this variable
+    // through the editor
+    public GameObject slingshotRockPrefab;
+    public float rockSpeed = 3f;
+
+	// Use this for initialization
+	void Start () 
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        FireSlingshot();
+	}
+
+    public void FireSlingshot()
+    {
+        // On enter press instantiate a rock projectile
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            GameObject rockProjectile = Instantiate(slingshotRockPrefab, gameObject.transform.position, Quaternion.identity);
+            rockProjectile.GetComponent<Rigidbody>().velocity = transform.forward * rockSpeed;
+            Destroy(rockProjectile, 5);
+        }
+    }
+}
