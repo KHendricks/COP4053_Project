80b5951c9e8b5daa80c538949b06df9eab99dd6a
Faye Strawn <strawnfaye@gmail.com>
2018-10-21 14:50:28 -0400

    working on enemy state machine

---
+++ COP4053/Assets/Scripts/GuardEnemy.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(Rigidbody))]
+public class GuardEnemy : Movement {
+
+    public Animator animator;
+    public StateManager<GuardEnemy> stateManager;
+    public int health;
+    public GameObject knifeForInventory;
+
+    // Use this for initialization
+    void Start () {
+        animator = GetComponentInChildren<Animator>();
+        stateManager = new StateManager<GuardEnemy>();
+        stateManager.Add("guard", new GuardState());
+
+        stateManager.Switch("guard");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        stateManager.Update(this);
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "SlingshotRock")
+        {
+            Destroy(other.gameObject);
+            health -= PlayerPrefs.GetInt("SlingshotDamage");
+            KillEnemy();
+        }
+    }
+
+    void KillEnemy()
+    {
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+
+            // If on first level let enemy drop knife
+            if (SceneManager.GetActiveScene().name == "Desert_level1")
+            {
+                DropKnife();
+            }
+        }
+    }
+
+    void DropKnife()
+    {
+        Instantiate(knifeForInventory, gameObject.transform.position, Quaternion.identity);
+    }
+}
---
+++ COP4053/Assets/Scripts/GuardState.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuardState : IState<GuardEnemy> {
+
+    public void OnEnter(GuardEnemy owner) 
+    {
+
+    }
+
+    public void Update(GuardEnemy owner)
+    {
+        owner.animator.Play("Idle");
+    }
+
+
+
+    public void OnExit(GuardEnemy owner)
+    {
+
+    }
+}
