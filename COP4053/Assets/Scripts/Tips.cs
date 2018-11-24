ffa965d21ad67d41cfc87ba81c69309badb470e8
Kyle <khendricks0815@gmail.com>
2018-11-24 03:59:22 -0500

    Added sign posts (with temp art) and a wandering enemy using a waypoint system
    - The wandering enemy is an extension of the guard enemy but with a new boolean value to trigger if he is to wander
    
    - There is currently a bug where the wander enemy will stop before the player and not attack him

---
+++ COP4053/Assets/Scripts/Tips.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tips : MonoBehaviour 
+{
+    public DialogMessage dialog;
+    public ContextMessage context;
+    public int tipIndex;
+
+    private List<string> tipList;
+	// Use this for initialization
+	void Start () 
+    {
+        tipList = new List<string>();
+        tipList.Add("Rescue the dog and find your slingshot.");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerExit(Collider other)
+    {
+        dialog.Deactivate();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (!dialog.active)
+        {
+            if (tipIndex >= 0)
+            {
+                dialog.Activate(tipList[tipIndex]);
+            }
+        }
+    }
+}
