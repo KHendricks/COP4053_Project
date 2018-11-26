7f9909fd36bbc1a2be595f6b5bfb9882476e3ec9
Kyle <khendricks0815@gmail.com>
2018-11-26 15:46:29 -0500

    Added temporary lasso weapon attack.
    - It is still a work in progress and causes the player to "jump" forward instead of lerping to the destination.

---
+++ COP4053/Assets/Scripts/LassoProjectile.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LassoProjectile : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag != "Lasso" && other.gameObject.tag != "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
