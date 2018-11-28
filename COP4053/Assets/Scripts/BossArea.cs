02e4e708e7390ddf4f560d13d9269433c883aebb
Kyle <khendricks0815@gmail.com>
2018-11-28 12:55:43 -0500

    Working on boss mechanics in new scene
    Had to adjust player script because of the way the flashing when hit worked before.

---
+++ COP4053/Assets/Scripts/BossArea.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossArea : MonoBehaviour 
+{
+
+    public GameObject boss;
+    private bool engaged;
+
+	// Use this for initialization
+	void Start () 
+    {
+        engaged = false;	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        // Keeps the collision area attached to boss' position
+        gameObject.transform.position = boss.transform.position;
+	}
+
+    public bool GetStatus()
+    {
+        return engaged;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            engaged = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            engaged = false;
+        }
+    }
+}
